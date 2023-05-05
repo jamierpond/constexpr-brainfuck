cb05ccee538b7e46f7bc535802ebf61c9647e981
Jamie Pond <jamiepond259@gmail.com>
2023-05-05 00:59:55 -0700

    run both examples

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "constexpr_brainfuck.hpp"
 
 int main() {
+    std::cout << interpretBrainfuck(helloWorld) << std::endl;
     std::cout << interpretBrainfuck(fib) << std::endl;
     return 0;
 }
